0c084eb76ca7846794f4d93b4b40fe8d9d5d94ec
Tiagomunerato <tiagomunerato@gmail.com>
2019-10-05 10:39:54 -0300

    Add project files.

---
+++ ex2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+
+            Console.Write("Informe o número para calcular a tabuada: ");
+            num = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine("{0} X {1} = {2}", num, i, (num * i));
+            }
+            Console.ReadKey();
+        }
+    }
+}
